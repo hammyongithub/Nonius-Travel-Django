ffb3cf0cbbfd60cacb1a41e8f86b75ec9746e915
Jorge Domingues <jorge@domingues.tech>
2023-12-08 16:29:16 +0000

    Redirect root URL to admin

--- django_backend/urls.py
+++ django_backend/urls.py
@@ -1,9 +1,15 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.shortcuts import redirect
+
+def redirect_to_admin(request):
+    return redirect('/admin/')
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/v1/', include('djoser.urls')),
     path('api/v1/', include('djoser.urls.authtoken')),
     path('api/v1/', include('nonius_travel.urls')),
+    path('', redirect_to_admin),  # Redirect root URL to admin
 ]
+
